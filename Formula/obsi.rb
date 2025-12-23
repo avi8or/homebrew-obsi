9f5991f46c0ad8788bbe69c9996dab413f1de831
Tyler Miller <tylermiller@Tylers-MacBook-Pro.local>
2025-12-22 22:16:35 -0700

    Update to v0.3.1 - multi-line input support for targets

--- Formula/obsi.rb
+++ Formula/obsi.rb
@@ -1,10 +1,10 @@
 class Obsi < Formula
   desc "CLI for Obsidian note-taking (Quick Notes Fork)"
   homepage "https://github.com/avi8or/obsidian-cli-quick-notes"
-  url "https://github.com/avi8or/obsidian-cli-quick-notes/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "fbdf71ad0be374afa9c397f9f8696865ef6a0c2fe3443bd03292adf3b0a45d83"
+  url "https://github.com/avi8or/obsidian-cli-quick-notes/archive/refs/tags/v0.3.1.tar.gz"
+  sha256 "ca994609750b980fa3551fb2845f8a42d8cd7a104fd808010861e6160184122e"
   license "MIT"
-  version "0.3.0"
+  version "0.3.1"
 
   depends_on "go" => :build
 
@@ -14,6 +14,6 @@ class Obsi < Formula
   end
 
   test do
-    assert_match "v0.3.0", shell_output("#{bin}/obsi --version")
+    assert_match "v0.3.1", shell_output("#{bin}/obsi --version")
   end
 end
